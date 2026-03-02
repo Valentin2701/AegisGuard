4e30b5136dca70fd67d5119eaaefc1982b7d5214
Valentin <valko27@abv.bg>
2026-03-02 12:19:56 +0200

    Run application without debug mode

--- backend/api/app.py
+++ backend/api/app.py
@@ -3,4 +3,4 @@ from . import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=8000)
+    socketio.run(app, debug=False, host='0.0.0.0', port=8000)
